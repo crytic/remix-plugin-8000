885091efc75253d969fe8c963d682cdbf61e6457
Ryan Stortz <ryan@withzombies.com>
2018-09-09 01:06:13 +0200

    solidity runs and reports info, but it needs to be pretty

--- server.py
+++ server.py
@@ -13,57 +13,57 @@ import pprint
 import tempfile
 import shlex
 
+import re
+import textwrap
+
 SOLC_PATH = os.environ.get('SOLC_PATH', '/usr/local/bin/solc')
 
-class MyServer(BaseHTTPServer.HTTPServer):
+def escape_ansi(line):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+    return ansi_escape.sub('', line)
 
+class MyServer(BaseHTTPServer.HTTPServer):
     def __init__(self):
         self.port = 8000
         SocketServer.TCPServer.__init__(self,('', self.port), MyHandler)
 
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
         BaseHTTPServer.HTTPServer.server_bind(self)
 
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         # do GETs as normal
-
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_slither(self, buf):
-
-        output = "{\"output\": \"SLITHER OUTPUT HERE\"}"
-
-
         info = json.loads(buf)
+
         # we get an array so get just first element
         info = info[0]
 
-        # get the current target:
-
-        #print info.keys() # source and data are keys
+        # get the current target
         sol_filename = info['source']['target']
 
         # TODO: add error handling in case the target is missing from sources
         source_code = info['source']['sources'][sol_filename]['content']
-
         
         ast = info['data']['sources'][sol_filename]['legacyAST']
         
         result = ''
-        with tempfile.NamedTemporaryFile(suffix='.json') as f:
-            f.write('== {} ==\n'.format(sol_filename))
-            f.write(json.dumps(ast))
-            f.flush()
+        with tempfile.NamedTemporaryFile(suffix='.json') as output_fp:
+            with tempfile.NamedTemporaryFile(suffix='.json') as f:
+                f.write('== {} ==\n'.format(sol_filename))
+                f.write(json.dumps(ast))
+                f.flush()
 
-            path = shlex.split('python ./slither/slither.py {} --solc-ast --disable-solc-warnings --solc {} --print-summary'.format(f.name, SOLC_PATH))
-            p = subprocess.Popen(path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            _, result = p.communicate()
-            p.wait()
-            
-        #pprint.pprint(info['data']['sources'][sol_filename]['legacyAST'])
+                path = shlex.split('python ./slither/slither.py {} --solc-ast --disable-solc-warnings --solc {} --json {}'.format(f.name, SOLC_PATH, output_fp.name))
+                p = subprocess.Popen(path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print p.communicate()
+                p.wait()
+
+                result = output_fp.read()   
+        
         #pprint.pprint(info['data']['contracts'][sol_filename])
         #print info['data']['contracts'][sol_filename].keys()
         
@@ -75,12 +75,10 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_header('Content-Length', str(len(output)))
         self.end_headers()
 
-
         self.wfile.write(output)
 
 
     def do_manticore(self, buf):
-
         output = {"status": 0, "output": "Manticore support coming soon"}
 
         output = json.dumps(output)
@@ -93,30 +91,19 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 
     def do_POST(self):
+        if self.path not in ('/slither', '/manticore', '/solium'):
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_POST(self)
+            return
+
         buf = ''
-        print dir(self)
-        print self.request
-        print self.requestline
-        print repr(self.headers)
-        print "path", self.path
-        #print self.rfile.read()
         content_len = int(self.headers.getheader('content-length', 0))
-        bytes_left = content_len
-        print bytes_left
-        post_body = ''
-        while bytes_left > 0:
-            new_buf = self.rfile.read(bytes_left)
-            post_body += new_buf
-            bytes_left -= len(new_buf)
-            print bytes_left
-        #post_body = self.rfile.readall(content_len)
+        while len(buf) < content_len:
+            buf += self.rfile.read(content_len - len(buf))
 
         if self.path == '/slither':
-            self.do_slither(post_body)
+            self.do_slither(buf)
         elif self.path == '/manticore':
-            self.do_manticore(post_body)
-
-        #SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+            self.do_manticore(buf)
 
 PORT = 8000
 
